12712f67695efed64d253f669ae64bd7a1be2bd1
colt05 <824ba30a@opayq.com>
2015-08-02 09:37:46 -0400

    Added converting back to 24 format

--- Class1.vb
+++ Class1.vb
@@ -63,7 +63,7 @@
             Return String.Concat(nTime, " ", AmPm)
         Else
             'Nothing needs to be done, because 12 is half of 24.
-            'Apply the variables.
+            'Apply the variab+les.
             tHr = tfHr
             'Add the : and minutes to the new time to apply to the 12 text box, and glitch fix.
             If tHr >= 10 Then
@@ -82,18 +82,88 @@
             'Return the new time.
             Return String.Concat(nTime, " ", AmPm)
         End If
-        Return "Error"
     End Function
+    ''' <summary>
+    ''' Convert the time from the 12 time format to the 24 time format.
+    ''' </summary>
+    ''' <param name="twelveformat">This is the 12 time format to be converted from.</param>
+    ''' <returns>The 24 time format string.</returns>
+    ''' <remarks></remarks>
+    Function convertTimeFromTwelve(twelveformat As TwelveFormatTime)
+        If twelveformat.AmOrPm = TwelveFormatTime.AMPMTime.AM Or twelveformat.AmOrPm = TwelveFormatTime.AMPMTime.ERR Then
+            Return String.Concat(twelveformat.Hour.ToString, ":", twelveformat.Minute.ToString, " AM")
+        ElseIf twelveformat.AmOrPm = TwelveFormatTime.AMPMTime.PM Then
+            Dim nHr As Integer = "24"
+            nHr = twelveformat.Hour + 12
+            Return String.Concat(nHr, ":", twelveformat.Minute.ToString)
+        Else
+            Return "Error"
+        End If
+    End Function
+    ''' <summary>
+    ''' This is used in convertTime() to tell it which type of time to convert from and which to convert to.
+    ''' </summary>
+    ''' <remarks></remarks>
+    Enum InputFormat
+        twelve = 1
+        twenty_four = 2
+    End Enum
 End Class
 Public Class InvalidTimeException
     Inherits Exception
-    Private MessageDim As String = String.Empty
+    Private Msg As String = String.Empty
     Public Sub New()
-        MessageDim = "The time is invalid."
+        Msg = "The time is invalid."
     End Sub
     Public Overrides ReadOnly Property Message As String
         Get
-            Return MessageDim
+            Return Msg
         End Get
     End Property
+End Class
+Public Class TwelveFormatTime
+    Private hr As Integer = "00"
+    Private min As Integer = "00"
+    Private ampm As AMPMTime = AMPMTime.AM
+    Public Overridable ReadOnly Property Hour As Integer
+        Get
+            Return hr
+        End Get
+    End Property
+            Public Overridable ReadOnly Property Minute As Integer
+        Get
+            Return min
+        End Get
+    End Property
+            Public Overridable ReadOnly Property AmOrPm As AMPMTime
+        Get
+            Return ampm
+        End Get
+    End Property
+    ''' <summary>
+    ''' Creates a new TwelveFormatTime for use with ConvertTime.
+    ''' </summary>
+    ''' <param name="HourTime">The integer of hours to be used (Must be double digits, 3 would turn into 03).</param>
+    ''' <param name="MinTime">The integer of minutes to be used (Must also be double digits).</param>
+    ''' <param name="IsAmOrPm">This is the enum for if the time is in the AM or the PM.</param>
+    ''' <remarks></remarks>
+    Public Sub New(HourTime As Integer, MinTime As Integer, IsAmOrPm As AMPMTime)
+        If HourTime > 12 Then
+            Throw New InvalidTimeException
+            Exit Sub
+        End If
+        hr = HourTime
+        If MinTime > 60 Then
+            Throw New InvalidTimeException
+            Exit Sub
+        End If
+        min = MinTime
+        'This doesn't need any checks for being invalid, Enums can be any of the options or null.
+        ampm = IsAmOrPm
+    End Sub
+    Enum AMPMTime
+        ERR = 0
+        AM = 1
+        PM = 2
+    End Enum
 End Class
