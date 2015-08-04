9570de83f410cb48e764ca420b3519c0118afbbb
colt05 <824ba30a@opayq.com>
2015-08-04 07:50:47 -0400

    Remove unused namespace name

--- Class1.vb
+++ Class1.vb
@@ -1,7 +1,7 @@
 ﻿Public Class TimeConvert
     ''' <summary>
     ''' Convert time from 24 Format to 12 Format.
-    ''' This function can throw a _24212C-DLL.InvalidTimeException because the time may be invalid, but will move on if it is given a is a valid time.
+    ''' This function can throw a InvalidTimeException because the time may be invalid, but will move on if it is given a is a valid time.
     ''' </summary>
     ''' <param name="toConvert">This is the time in 24 Format that will be converted. This must include a : between the hours and the minutes, or else the conversion will fail.</param>
     ''' <param name="forceConvert">Set this to true if you want to force the conversion without throwing InvalidTimeExceptions.</param>
